2ed716eb713b7ffd458b354ed62f9caaaef75cbe
Vaibhav <kasarvaibhav777@gmail.com>
2022-08-30 14:59:43 +0530

    assgn completed with jwt

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,24 +6,36 @@ const userModel = require("../models/userModel");
 
 
 const authorise = function(req, res, next) {
-   
+    let token = req.headers["x-Auth-token"]||req.headers["x-auth-token"];  //case sensitive
+    //   if (!token) token = req.headers["x-auth-token"];
+    
+      //If no token is present in the request header return error. This means the user is not logged in.
+      if (!token) return res.send({ status: false, msg: "token must be present" });
+    
+      //try & catch is used for error handling
+      try{
+        let decoded = jwt.verify(token,'functionup-thorium')
+      }
+    
+    catch(error){
+     //console.log(error)
+        return res.send({msg:error.message})
+      }
 
-    let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-  
-  let decodedToken = jwt.verify(token, "functionup-thorium");
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
-  
-    let userToBeModified = req.params.userId
-    let userLoggedIn = decodedToken.userId
-   if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
-
-    let user =  userModel.findById(req.params.userId)
-    if(!user) return res.send({status: false, msg: 'No such user exists'})
-    next()
-}
-
+      
+      try{
+        let decoded =  jwt.verify(token,'functionup-thorium')
+        let userToModify = req.params.userId
+        let userLoggedIn= decoded.userId
+        if(userToModify!=userLoggedIn){
+          return res.send({msg: " UnAuthorized User !"})
+        }else{userId}
+      }
+    
+    
+      catch (error){}
+    
+      next()
+    }
 
 module.exports.authorise=authorise
